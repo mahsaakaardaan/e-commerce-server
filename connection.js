94dfbc60bb07183effeb1d2dfb78e01196b2194d
mahsa.kardan <mahsa.kardan@digikala.com>
2025-11-08 14:48:43 +0330

    hh

--- connection.js
+++ connection.js
@@ -2,12 +2,25 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: 'root',
-  password: 'root',
-  database: process.env.DATA_BASE,
-  port: process.env.DB_PORT_2,
+  // host: process.env.HOST,
+  // user: 'moonio_mahsaa',
+  // password: '123456789',
+  // database: process.env.DATA_BASE,
+  // port: process.env.DB_PORT_2,
+  // multipleStatements: true
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  // port: process.env.DB_PORT_2,
   multipleStatements: true
+
+  // host: '46.34.163.193',
+  // user: 'mahsa_node',
+  // password: 'StrongPass123',
+  // database:'ecommerce',
+  // // port: process.env.DB_PORT_2,
+  // multipleStatements: true
 });
 
 db.connect((err) => { 
